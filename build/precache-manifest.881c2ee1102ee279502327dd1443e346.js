a12ae6a5b2f98bc2467fe4f9c5b46e27edc337d9
Blex <iamlex.yk@gmail.com>
2024-08-08 16:23:50 +0800

    deploy

---
+++ build/precache-manifest.881c2ee1102ee279502327dd1443e346.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064b67ccb40d5a09e4166ad30ce76489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43ee703c74ef2ad8ec81",
+    "url": "/static/css/2.1eb21bb0.chunk.css"
+  },
+  {
+    "revision": "6ca691d4fdc4d884c945",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "43ee703c74ef2ad8ec81",
+    "url": "/static/js/2.cef14931.chunk.js"
+  },
+  {
+    "revision": "db5ae40f7b911042cc0cfc6811e6506e",
+    "url": "/static/js/2.cef14931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca691d4fdc4d884c945",
+    "url": "/static/js/main.86b9e4ba.chunk.js"
+  },
+  {
+    "revision": "f328827c89c8be2430c8",
+    "url": "/static/js/runtime-main.c7215816.js"
+  },
+  {
+    "revision": "445ae065a7b45bab3ce0fce367bd678f",
+    "url": "/static/media/clothes-red.445ae065.jpg"
+  },
+  {
+    "revision": "93f534e91e622de9363dad2bdf7117aa",
+    "url": "/static/media/gap.93f534e9.jpg"
+  },
+  {
+    "revision": "e5d632a9101716aab330ec2bff8d4858",
+    "url": "/static/media/test-blue.e5d632a9.png"
+  },
+  {
+    "revision": "d666196b8696b65668b9a32cf9a69423",
+    "url": "/static/media/test-green.d666196b.png"
+  },
+  {
+    "revision": "966d5510f26c43876f368dd7b729e4f0",
+    "url": "/static/media/test-red.966d5510.png"
+  },
+  {
+    "revision": "c6a97398d28a7a230f62e69989d10224",
+    "url": "/static/media/tshirt.c6a97398.jpg"
+  },
+  {
+    "revision": "4ff111de0a2434a8790843e9bd99f936",
+    "url": "/static/media/uniqlo-model.4ff111de.png"
+  },
+  {
+    "revision": "c39106a39e1918ded991900521a16138",
+    "url": "/static/media/uniqlo-model2.c39106a3.jpg"
+  },
+  {
+    "revision": "14450dad6214556fd055fad5f4145da6",
+    "url": "/static/media/uniqlo-purple.14450dad.png"
+  },
+  {
+    "revision": "a85775a125d85f05a89db43bf5be8007",
+    "url": "/static/media/uniqlo-short-gray.a85775a1.png"
+  }
+]);
